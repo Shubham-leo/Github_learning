9fb59ac85f47f44c826254ea25fe51c76bb4fcc3
Shubham-leo <shubham.in10@gmail.com>
2024-05-13 19:03:33 +0530

    updated code functions

--- interchange first and last elements.py
+++ interchange first and last elements.py
@@ -5,19 +5,19 @@ Output : [24, 35, 9, 56, 12]
 Input : [1, 2, 3]
 Output : [3, 2, 1]"""
 
-def interchange_FEwithLE(input_list):
+def interchange_list(input_list):
     lenth = len(input_list)
     temp = input_list[0]
     input_list[0] = input_list[lenth-1]
     input_list[lenth-1]=temp
     return input_list
-def interchange_FEwithLE2(input_list):
+def interchange_list(2input_list):
     get = input_list[0],input_list[-1]
     input_list[-1],input_list[0] = get
     print(get)
     return input_list
-def interchange_FEwithLE3(newList):
+def interchange_list3(newList):
     newList[0], newList[-1] = newList[-1], newList[0]
     return newList
 Input = [12, 35, 9, 56, 24]
-print(interchange_FEwithLE3(Input))
+print(interchange_list3(Input))
