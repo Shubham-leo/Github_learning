f44e0840c92196c4e0ec8c7b950d54a726d87efe
Shubham-leo <shubham.in10@gmail.com>
2024-06-07 01:09:22 +0530

    added folder

---
+++ foodmate/count_unique.py
@@ -0,0 +1,20 @@
+input_list= [1,4,3,4,3,2,2,2,1,6,7,8]
+
+
+hashset = set()
+lst2 = []
+count = 0
+for n in input_list:
+    if n not in lst2:
+        count+=1
+        lst2.append(n)
+
+print(count)
+
+from collections import Counter
+
+items = Counter(input_list).keys()
+
+print(len(items))
+
+input_list.unique()
