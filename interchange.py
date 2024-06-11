b9c5ad86bcbc435be5fd127040dc20bcc6ea4d3d
Shubham-leo <shubham.in10@gmail.com>
2024-06-12 00:43:25 +0530

    new

--- interchange.py
+++ interchange.py
@@ -26,8 +26,11 @@ Input = [12, 35, 9, 56, 24,23,1234]
 print(interchange_list2(Input))
 print(interchange_list2(Input))
 <<<<<<< HEAD
+<<<<<<< HEAD
 #created confilct
 =======
+=======
+>>>>>>> parent of f62c2d9 (created revert)
 #created sentences to revert
 
 >>>>>>> parent of f62c2d9 (created revert)
